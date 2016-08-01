1bc290ca26ef397d3cc4b18d331708d1629a1a68
Stefan Walther <Stefan.Walther@qlik.com>
2016-08-01 22:25:39 +0200

    feat(sc-datepicker): Binding some basic variables, basic doc

---
+++ docs/sc-datepicker.md
@@ -0,0 +1,19 @@
+## DatePicker
+
+> Datepicker component
+
+### Basic Usage
+
+## Properties
+
+- disable
+- inline
+- maxDate
+- minDate
+- time_24hr
+- weekNumbers
+- 
+
+## Credits
+
+All credits go to [Gregory](https://github.com/chmln), who created the fantastic [Flatpickr](https://github.com/chmln/flatpickr) component, which served as a basis for this project.
--- src/sc-datepicker/sc-datepicker.js
+++ src/sc-datepicker/sc-datepicker.js
@@ -1,33 +1,50 @@
+/**
+ * @todo
+ * - allowInput: doesn't seem to work ...
+ */
+/*global define*/
 define( [
 	"angular",
 	"jquery",
 	"underscore",
+	"text!./sc-datepicker.ng.html",
 	"./external/flatpickr/flatpickr.min",
-	"css!./external/flatpickr/flatpickr.min.css",
+	"css!./external/flatpickr/flatpickr.material_orange.min.css",
 	"css!./sc-flatpickr-style-override.css"
-], function ( angular, $, _ ) {
+], function ( angular, $, _, ngTemplate ) {
 	'use strict';
+
+	function isTrue ( o ) {
+		return o && o.toLowerCase() === 'true';
+	}
 
 	return {
 		name: "scDatepicker",
 		restrict: 'E',
 		replace: true,
-		template: '<input class="lui-input scDatepicker" />',
+		template: ngTemplate,
 		scope: {
-			date: '=',
-			width: "@"
+			width: '@',				// Width of the input element
+			inline: '='
 		},
-		link: function ( scope, elem ) {
+		link: function ( scope, elem, attrs ) {
 
-			var opts = {};
+			var opts = {
+				allowInput: false,
+				weekNumbers: scope.$eval( attrs.weekNumbers ),
+				inline: scope.inline
+			};
+			// console.log('opts', opts);
 
+			// Element styling
 			elem.css( 'width', scope.width );
 
 			var e = angular.element( elem )[0];
-			var datePickr = new window.flatpickr( e, opts );
+			var datePickr = new Flatpickr( e, opts );
 
 			datePickr.config.onChange = function ( dateObj ) {
-				console.log( dateObj );
+				scope.selectedDate = dateObj;
+				//Todo: Set to variable or whatever
 			};
 
 		}
