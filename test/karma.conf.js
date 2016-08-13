87a158a82a3fc83406c00f639cdf2f3d4e217cd3
Stefan Walther <Stefan.Walther@qlik.com>
2016-08-13 13:46:13 +0200

    test: Go back to log only info & disable autowatch

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -61,10 +61,10 @@ module.exports = function ( config ) {
 
 		// level of logging
 		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-		logLevel: config.LOG_DEBUG,
+		logLevel: config.LOG_INFO,
 
 		// enable / disable watching file and executing tests whenever any file changes
-		autoWatch: true,
+		autoWatch: false,
 
 		// start these browsers
 		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
